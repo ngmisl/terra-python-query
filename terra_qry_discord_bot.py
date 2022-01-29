cb9ed54d13b6aad56e999528a138c92a65328b2a
ngmisl <98217124+ngmisl@users.noreply.github.com>
2022-01-29 07:37:45 +0100

    Update terra_qry_discord_bot.py

--- terra_qry_discord_bot.py
+++ terra_qry_discord_bot.py
@@ -2,26 +2,12 @@ import discord
 import requests
 import os
 
-# create .env with discord bot token
 my_secret = os.environ['TOKEN']
 
-# discord client
-client = discord.Client()
-
-# terra query start
 glow = 'terra1tu9yjssxslh3fd6fe908ntkquf3nd3xt8kp2u2'
 
-query_msg = '{"state":{"contract_addr":"' + glow + '"}}'
+client = discord.Client()
 
-response = requests.get(
-    "https://lcd.terra.dev/wasm/contracts/" + glow + "/store",
-    params={"query_msg": query_msg},
-).json()
-
-total_tickets = response['result']['total_tickets']
-# terra query end
-
-# discord code start 
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
@@ -32,7 +18,13 @@ async def on_message(message):
     return
   
   if message.content.startswith('$tickets'):
+    query_msg = '{"state":{"contract_addr":"' + glow + '"}}'
+    response = requests.get("https://lcd.terra.dev/wasm/contracts/" + glow + "/store", 
+    params={"query_msg": query_msg},
+    ).json()
+    
+    total_tickets = response['result']['total_tickets']
+
     await message.channel.send('Total Tickets Locked: ' + total_tickets)
 
 client.run(my_secret)
-# discord code end
